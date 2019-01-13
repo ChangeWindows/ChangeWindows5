48e3a1bc6b1e1f6dffd07511b0a47bfcf4e58723
Yannick <yannick.franssen@outlook.com>
2019-01-13 16:47:28 +0100

    #1 Add colors and names, design update

--- resources/views/milestones.blade.php
+++ resources/views/milestones.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.app')
 
+@section('hero')
+<div class="jumbotron">
+    <div class="container">
+        <h2>Milestones</h2>
+    </div>
+</div>
+@endsection
 
 @section('content')
 <div class="row">
     <div class="col">
-        <h2>Milestones</h2>
-        @foreach ($milestones as $milestone)
-            <h4><i class="fab fa-fw fa-windows"></i> {{ $milestone->osname }} version {{ $milestone->version }}</h4>
-        @endforeach
+        <div class="row">
+            @foreach ($milestones as $milestone)
+                <div class="col-lg-4 col-md-6 col-12">
+                    <a class="milestone" href="#">
+                        <h4 class="text-center" style="color: #{{ $milestone->color }}">
+                            <i class="fab fa-fw fa-windows"></i> <span class="font-weight-bold">{{ $milestone->osname }}</span>
+                        </h4>
+                        <h3 class="text-center" style="color: #{{ $milestone->color }}">
+                            {{ $milestone->name }}
+                        </h3>
+                    </a>
+                </div>
+            @endforeach
+        </div>
     </div>
 </div>
 @endsection
