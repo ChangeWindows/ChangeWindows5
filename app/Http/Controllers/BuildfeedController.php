614746ca915a43e02f72c09d755a5e3a9fd8e45f
Yannick <yannick.franssen@outlook.com>
2019-01-15 21:49:46 +0100

    Properly define the build sorting

--- app/Http/Controllers/BuildfeedController.php
+++ app/Http/Controllers/BuildfeedController.php
@@ -8,7 +8,7 @@ use App\Buildfeed;
 class BuildfeedController extends Controller
 {
     public function index() {
-        $builds = Buildfeed::paginate(50)->onEachSide(1);
+        $builds = Buildfeed::orderBy('major', 'ASC')->orderBy('minor', 'ASC')->orderBy('build', 'ASC')->orderBy('revision', 'ASC')->paginate(10)->onEachSide(1);
 
         return view('buildfeed.index', compact('builds'));
     }
