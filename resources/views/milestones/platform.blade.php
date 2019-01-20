98aaa329d47b51033a48cfc260557dd17b681f56
Yannick <yannick.franssen@outlook.com>
2019-01-20 22:47:31 +0100

    #1 Proper structure for platform details

--- resources/views/milestones/platform.blade.php
+++ resources/views/milestones/platform.blade.php
@@ -44,13 +44,46 @@
         <div class="timeline">
             @foreach ($timeline as $build => $rings)
                 <div class="timeline-row">
-                    <a class="row" href="{{ URL::to('build/'.$build.'/'.$platform_id) }}">
+                    <a class="row" href="{{ URL::to('build/'.explode('.', $build)[0].'/'.$platform_id) }}">
                         <div class="col-6 col-md-2 build"><img src="{{ asset('img/platform/'.getPlatformImage($platform_id)) }}" class="img-platform img-jump" alt="{{ getPlatformById($platform_id) }}" />{{ $build }}</div>
-                        @foreach ($rings as $ring_id => $date)
-                            <div class="col-6 col-md-2 ring">
-                                <span class="label {{ getRingClassById($ring_id) }}">{{ $date }}</span>
-                            </div>
-                        @endforeach
+                        @if (in_array($platform_id, [1, 3]))
+                        <div class="col ring">
+                            <span class="label skip">{{ array_key_exists('1', $rings) ? $rings['1'] : '' }}</span>
+                        </div>
+                        @endif
+                        @if (in_array($platform_id, [1, 2, 3]))
+                        <div class="col ring">
+                            <span class="label fast">{{ array_key_exists('2', $rings) ? $rings['2'] : '' }}</span>
+                        </div>
+                        @endif
+                        @if (in_array($platform_id, [1, 2, 3, 4, 5, 6, 7]))
+                        <div class="col ring">
+                            <span class="label slow">{{ array_key_exists('3', $rings) ? $rings['3'] : '' }}</span>
+                        </div>
+                        @endif
+                        @if (in_array($platform_id, [3]))
+                        <div class="col ring">
+                            <span class="label preview">{{ array_key_exists('4', $rings) ? $rings['4'] : '' }}</span>
+                        </div>
+                        @endif
+                        @if (in_array($platform_id, [1, 2, 3]))
+                        <div class="col ring">
+                            <span class="label release">{{ array_key_exists('5', $rings) ? $rings['5'] : '' }}</span>
+                        </div>
+                        @endif
+                        <div class="col ring">
+                            <span class="label targeted">{{ array_key_exists('6', $rings) ? $rings['6'] : '' }}</span>
+                        </div>
+                        @if (in_array($platform_id, [1, 2, 5, 6, 7]))
+                        <div class="col ring">
+                            <span class="label broad">{{ array_key_exists('7', $rings) ? $rings['7'] : '' }}</span>
+                        </div>
+                        @endif
+                        @if (in_array($platform_id, [1, 4, 5]))
+                        <div class="col ring">
+                            <span class="label ltsc">{{ array_key_exists('8', $rings) ? $rings['8'] : '' }}</span>
+                        </div>
+                        @endif
                     </a>
                 </div>
             @endforeach
