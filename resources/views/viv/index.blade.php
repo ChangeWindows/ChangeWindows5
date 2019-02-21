2091a586be7da82de4be1212712dc2aaa0ae8b65
Yannick <yannick.franssen@outlook.com>
2019-02-21 22:22:28 +0100

    #27 Revamp our Patreon design

--- resources/views/viv/index.blade.php
+++ resources/views/viv/index.blade.php
@@ -14,39 +14,42 @@
         <h2>Patrons</h2>
         <a href="https://www.patreon.com/bePatron?c=1028298" class="btn btn-primary btn-patreon"><i class="fab fa-fw fa-patreon"></i> Become a Patron</a>
         <p>These are our donators, they help us make ChangeWindows a reality. If you want to join them in this, feel free to click the 'Become a Patron' button.</p>
-        <h2>Gold</h2>
-        <div class="row">
-            @foreach ($gold as $patron)
-                <div class="col-3">
-                    <div class="patron gold">
-                        <div class="header">Insider Gold</div>
-                        <i class="fal fa-fw fa-user-circle"></i>
-                        <div class="name">{{ $patron->name }}</div>
+        <div class="jumbotron insider gold">
+            <h1 class="text-center text-lowercase">Insider<span class="font-weight-light">Gold</span></h1>
+            <div class="row justify-content-md-center">
+                @foreach ($gold as $patron)
+                    <div class="col-3">
+                        <div class="patron">
+                            <i class="fal fa-fw fa-user-circle"></i>
+                            <div class="name">{{ $patron->name }}</div>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-        <h2>Silver</h2>
-        <div class="row">
-            @foreach ($silver as $patron)
-                <div class="col-3">
-                    <div class="patron silver">
-                        <div class="header">Insider Silver</div>
-                        <div class="name">{{ $patron->name }}</div>
+        <div class="jumbotron insider silver">
+            <h1 class="text-center text-lowercase">Insider<span class="font-weight-light">Silver</span></h1>
+            <div class="row justify-content-md-center">
+                @foreach ($silver as $patron)
+                    <div class="col-3">
+                        <div class="patron">
+                            <div class="name">{{ $patron->name }}</div>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
-        <h2>Bronze</h2>
-        <div class="row">
-            @foreach ($bronze as $patron)
-                <div class="col-3">
-                    <div class="patron bronze">
-                        <div class="header">Insider Bronze</div>
-                        <div class="name">{{ $patron->name }}</div>
+        <div class="jumbotron insider bronze">
+            <h1 class="text-center text-lowercase">Insider<span class="font-weight-light">Bronze</span></h1>
+            <div class="row justify-content-md-center">
+                @foreach ($bronze as $patron)
+                    <div class="col-3">
+                        <div class="patron">
+                            <div class="name">{{ $patron->name }}</div>
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
 </div>
