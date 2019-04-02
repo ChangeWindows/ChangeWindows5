70c4aad45fb324831d8260991dea5e30d8751dc6
Yannick <yannick.franssen@outlook.com>
2019-04-02 22:08:35 +0200

    #62 Update seeds

--- database/seeds/RoleTableSeeder.php
+++ database/seeds/RoleTableSeeder.php
@@ -13,43 +13,43 @@ class RoleTableSeeder extends Seeder
     public function run()
     {
         $admin = new Role();
-        $admin->rank = 1;
+        $admin->id = 1;
         $admin->name = 'Admin';
         $admin->description = 'Administrates ChangeWindows';
         $admin->save();
         
         $editor = new Role();
-        $editor->rank = 2;
+        $editor->id = 2;
         $editor->name = 'Editor';
         $editor->description = 'Users with editing permissions';
         $editor->save();
     
         $platinum = new Role();
-        $platinum->rank = 10;
+        $platinum->id = 10;
         $platinum->name = 'Platinum Insider';
         $platinum->description = 'Insiders who contribute to and test ChangeWindows';
         $platinum->save();
     
         $gold = new Role();
-        $gold->rank = 11;
+        $gold->id = 11;
         $gold->name = 'Gold Insider';
         $gold->description = 'Insiders who contribute to and test ChangeWindows';
         $gold->save();
     
         $silver = new Role();
-        $silver->rank = 12;
+        $silver->id = 12;
         $silver->name = 'Silver Insider';
         $silver->description = 'Insiders who contribute to ChangeWindows';
         $silver->save();
     
         $bronze = new Role();
-        $bronze->rank = 13;
+        $bronze->id = 13;
         $bronze->name = 'Bronze Insider';
         $bronze->description = 'Insiders who contribute to ChangeWindows';
         $bronze->save();
     
         $user = new Role();
-        $user->rank = 20;
+        $user->id = 20;
         $user->name = 'User';
         $user->description = 'User with a user account';
         $user->save();
--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -13,29 +13,25 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        $role_admin = Role::where('name', 'Admin')->first();
-        $role_insider = Role::where('name', 'Platinum Insider')->first();
-        $role_user = Role::where('name', 'User')->first();
-    
         $admin = new User();
         $admin->name = 'Yannick';
         $admin->email = 'yannick@changewindows.org';
         $admin->password = bcrypt('secret');
+        $admin->role_id = 1;
         $admin->save();
-        $admin->roles()->attach($role_admin);
     
         $insider = new User();
         $insider->name = 'Viv';
         $insider->email = 'viv@changewindows.org';
         $insider->password = bcrypt('secret');
+        $admin->role_id = 10;
         $insider->save();
-        $insider->roles()->attach($role_insider);
     
         $user = new User();
         $user->name = 'Tom';
         $user->email = 'tom@changewindows.org';
         $user->password = bcrypt('secret');
+        $admin->role_id = 20;
         $user->save();
-        $user->roles()->attach($role_user);
     }
 }
