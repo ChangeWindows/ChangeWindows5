3ebec47a042f475d7156b8b5e32ce0562661ca02
Yannick <yannick.franssen@outlook.com>
2019-11-26 18:43:04 +0100

    Update dependencies

--- resources/views/viv/changelog.blade.php
+++ resources/views/viv/changelog.blade.php
@@ -4,6 +4,13 @@
 @section('content')
 <h2>What's new</h2>
 <section class="release-notes">
+    <h3>5.0.6 <small>26 November 2019</small></h3>
+    <h4><i class="fad fa-fw fa-exchange"></i> Changed</h4>
+    <p>Update to Bootstrap 4.4</p>
+    <h4><i class="fad fa-fw fa-band-aid"></i> Fixed</h4>
+    <p>Fixes the random sorting order for builds in the timeline</p>
+</section>
+<section class="release-notes">
     <h3>5.0.5 <small>17 November 2019</small></h3>
     <h4><i class="fad fa-fw fa-exchange"></i> Changed</h4>
     <p>The Timeline has now a more compact layout and shows 75 releases per page instead of 50</p>
