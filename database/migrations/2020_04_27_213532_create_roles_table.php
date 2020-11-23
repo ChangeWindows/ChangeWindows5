b78a18b6d965a351f79ecc75e105f41b8cd38621
Yannick <yannick.franssen@outlook.com>
2020-11-23 20:08:21 +0100

    Fix migration errors

--- database/migrations/2020_04_27_213532_create_roles_table.php
+++ database/migrations/2020_04_27_213532_create_roles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRoleTable extends Migration {
+class CreateRolesTable extends Migration {
     /**
      * Run the migrations.
      *
