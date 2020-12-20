cd2735ab6626c2257a8d9043da0dc80bef2daaf7
Yannick <yannick.franssen@outlook.com>
2020-12-20 21:55:40 +0100

    Update Channels overview

--- app/Http/Controllers/RingsController.php
+++ app/Http/Controllers/RingsController.php
@@ -5,48 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Release;
 use App\Milestone;
+use App\Platform;
 use App\MilestonePlatform;
 use App\ChannelPlatform;
 
 class RingsController extends Controller
 {
     public function index() {
-        $flights['pc']['fast'] = Release::pc()->active()->latestFlight()->first();
-        $flights['pc']['slow'] = Release::pc()->slow()->latestFlight()->first();
-        $flights['pc']['release'] = Release::pc()->release()->latestFlight()->first();
-        $flights['pc']['targeted'] = Release::pc()->targeted()->latestFlight()->first();
-        $flights['pc']['broad'] = Release::pc()->broad()->latestFlight()->first();
-        $flights['pc']['ltsc'] = Release::pc()->ltsc()->latestFlight()->first();
+        $platforms = Platform::all();
+        $flights = [];
 
-        $flights['xbox']['skip'] = Release::xbox()->skip()->latestFlight()->first();
-        $flights['xbox']['fast'] = Release::xbox()->active()->latestFlight()->first();
-        $flights['xbox']['slow'] = Release::xbox()->slow()->latestFlight()->first();
-        $flights['xbox']['preview'] = Release::xbox()->preview()->latestFlight()->first();
-        $flights['xbox']['release'] = Release::xbox()->release()->latestFlight()->first();
-        $flights['xbox']['targeted'] = Release::xbox()->targeted()->latestFlight()->first();
-
-        $flights['server']['slow'] = Release::server()->slow()->latestFlight()->first();
-        $flights['server']['targeted'] = Release::server()->targeted()->latestFlight()->first();
-        $flights['server']['ltsc'] = Release::server()->ltsc()->latestFlight()->first();
-
-        $flights['iot']['targeted'] = Release::iot()->targeted()->latestFlight()->first();
-        $flights['iot']['broad'] = Release::iot()->broad()->latestFlight()->first();
-
-        $flights['holo']['fast'] = Release::holographic()->active()->latestFlight()->first();
-        $flights['holo']['slow'] = Release::holographic()->slow()->latestFlight()->first();
-        $flights['holo']['targeted'] = Release::holographic()->targeted()->latestFlight()->first();
-        $flights['holo']['broad'] = Release::holographic()->broad()->latestFlight()->first();
-        $flights['holo']['ltsc'] = Release::holographic()->ltsc()->latestFlight()->first();
-
-        $flights['team']['fast'] = Release::team()->active()->latestFlight()->first();
-        $flights['team']['slow'] = Release::team()->slow()->latestFlight()->first();
-        $flights['team']['targeted'] = Release::team()->targeted()->latestFlight()->first();
-        $flights['team']['broad'] = Release::team()->broad()->latestFlight()->first();
-
-        $flights['tenx']['slow'] = Release::tenX()->slow()->latestFlight()->first();
-
-        $flights['sdk']['targeted'] = Release::sdk()->targeted()->latestFlight()->first();
-        $flights['iso']['targeted'] = Release::iso()->targeted()->latestFlight()->first();
+        foreach($platforms as $platform) {
+            foreach($platform->channelPlatforms as $channelPlatform) {
+                if ($channelPlatform->active === 1) {
+                    $flights[$platform->slug][$channelPlatform->channel->slug] = Release::where('platform', '=', $platform->id)
+                                            ->where('ring', '=', $channelPlatform->channel->id)
+                                            ->orderBy('build', 'desc')
+                                            ->orderBy('delta', 'desc')
+                                            ->orderBy('date', 'desc')
+                                            ->first();
+                }
+            }
+        }
 
         return view('rings.index', compact('flights'));
     }
--- resources/views/rings/index.blade.php
+++ resources/views/rings/index.blade.php
@@ -11,7 +11,7 @@
                 <a class="nav-link {{ Request::is('rings/pc') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'pc']) }}">PC</a>
                 <a class="nav-link {{ Request::is('rings/xbox') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'xbox']) }}">Xbox</a>
                 <a class="nav-link {{ Request::is('rings/server') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'server']) }}">Server</a>
-                <a class="nav-link {{ Request::is('rings/tenx') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'tenx']) }}">10X</a>
+                <a class="nav-link {{ Request::is('rings/10x') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => '10x']) }}">10X</a>
                 <a class="nav-link {{ Request::is('rings/holographic') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'holographic']) }}">Holographic</a>
                 <a class="nav-link {{ Request::is('rings/team') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'team']) }}">Team</a>
                 <a class="nav-link {{ Request::is('rings/iot') ? 'active' : '' }}" href="{{ route('platformRings', ['platform' => 'iot']) }}">IoT</a>
@@ -26,75 +26,75 @@
     <div class="col-12" id="pc">
         <p class="h3 pc">{!! getPlatformIconNoStyle(1) !!} PC</p>
         <div class="row row-gutter">
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['fast'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['slow'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['release'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['targeted'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['broad'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['ltsc'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['dev'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['beta'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['release-preview'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['sa'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['sa-b'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['pc']['lts'] ) ?></div>
         </div>
     </div>
     <div class="col-12" id="xbox">
         <p class="h3 xbox">{!! getPlatformIconNoStyle(3) !!} Xbox</p>
         <div class="row row-gutter">
             <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['skip'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['fast'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['slow'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['dev'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['beta'] ) ?></div>
             <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['preview'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['release'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['targeted'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['release-preview'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['xbox']['sa'] ) ?></div>
         </div>
     </div>
     <div class="col-lg-6" id="server">
         <p class="h3 server">{!! getPlatformIconNoStyle(4) !!} Server</p>
         <div class="row row-gutter">
-            <div class="col-xl col-sm-4 col-6"><?php getTile( $flights['server']['slow'] ) ?></div>
-            <div class="col-xl col-sm-4 col-6"><?php getTile( $flights['server']['targeted'] ) ?></div>
-            <div class="col-xl col-sm-4 col-12"><?php getTile( $flights['server']['ltsc'] ) ?></div>
+            <div class="col-xl col-sm-4 col-6"><?php getTile( $flights['server']['beta'] ) ?></div>
+            <div class="col-xl col-sm-4 col-6"><?php getTile( $flights['server']['sa'] ) ?></div>
+            <div class="col-xl col-sm-4 col-12"><?php getTile( $flights['server']['lts'] ) ?></div>
         </div>
     </div>
-    <div class="col-lg-2" id="tenx">
-        <p class="h3 tenx">{!! getPlatformIconNoStyle(10) !!} 10X</p>
+    <div class="col-lg-2" id="10x">
+        <p class="h3 10x">{!! getPlatformIconNoStyle(10) !!} 10X</p>
         <div class="row row-gutter">
-            <div class="col"><?php getTile( $flights['tenx']['slow'] ) ?></div>
+            <div class="col"><?php getTile( $flights['10x']['beta'] ) ?></div>
         </div>
     </div>
     <div class="col-lg-4" id="iot">
         <p class="h3 iot">{!! getPlatformIconNoStyle(6) !!} IoT</p>
         <div class="row row-gutter">
-            <div class="col-xl col-6"><?php getTile( $flights['iot']['targeted'] ) ?></div>
-            <div class="col-xl col-6"><?php getTile( $flights['iot']['broad'] ) ?></div>
+            <div class="col-xl col-6"><?php getTile( $flights['iot']['sa'] ) ?></div>
+            <div class="col-xl col-6"><?php getTile( $flights['iot']['sa-b'] ) ?></div>
         </div>
     </div>
     <div class="col-12" id="holographic">
         <p class="h3 holographic">{!! getPlatformIconNoStyle(5) !!} Holographic</p>
         <div class="row row-gutter">
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['holo']['fast'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['holo']['slow'] ) ?></div>
-            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['holo']['targeted'] ) ?></div>
-            <div class="col-md col-sm-6 col-6"><?php getTile( $flights['holo']['broad'] ) ?></div>
-            <div class="col-md col-sm-6 col-12"><?php getTile( $flights['holo']['ltsc'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['holographic']['dev'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['holographic']['beta'] ) ?></div>
+            <div class="col-md col-sm-4 col-6"><?php getTile( $flights['holographic']['sa'] ) ?></div>
+            <div class="col-md col-sm-6 col-6"><?php getTile( $flights['holographic']['sa-b'] ) ?></div>
+            <div class="col-md col-sm-6 col-12"><?php getTile( $flights['holographic']['lts'] ) ?></div>
         </div>
     </div>
     <div class="col-lg-8 col-12" id="team">
         <p class="h3 team">{!! getPlatformIconNoStyle(7) !!} Team</p>
         <div class="row row-gutter">
-            <div class="col-md col-6"><?php getTile( $flights['team']['fast'] ) ?></div>
-            <div class="col-md col-6"><?php getTile( $flights['team']['slow'] ) ?></div>
-            <div class="col-md col-6"><?php getTile( $flights['team']['targeted'] ) ?></div>
-            <div class="col-md col-6"><?php getTile( $flights['team']['broad'] ) ?></div>
+            <div class="col-md col-6"><?php getTile( $flights['team']['dev'] ) ?></div>
+            <div class="col-md col-6"><?php getTile( $flights['team']['beta'] ) ?></div>
+            <div class="col-md col-6"><?php getTile( $flights['team']['sa'] ) ?></div>
+            <div class="col-md col-6"><?php getTile( $flights['team']['sa-b'] ) ?></div>
         </div>
     </div>
     <div class="col-lg-2 col-6" id="sdk">
         <p class="h3 sdk">{!! getPlatformIconNoStyle(9) !!} SDK</p>
         <div class="row row-gutter">
-            <div class="col"><?php getTile( $flights['sdk']['targeted'] ) ?></div>
+            <div class="col"><?php getTile( $flights['sdk']['sa'] ) ?></div>
         </div>
     </div>
     <div class="col-lg-2 col-6" id="iso">
         <p class="h3 iso">{!! getPlatformIconNoStyle(8) !!} ISO</p>
         <div class="row row-gutter">
-            <div class="col"><?php getTile( $flights['iso']['targeted'] ) ?></div>
+            <div class="col"><?php getTile( $flights['iso']['sa'] ) ?></div>
         </div>
     </div>
 </div>
